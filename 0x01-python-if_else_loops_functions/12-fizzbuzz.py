a747055826591d2d11c017f7ce650df4c8056d4d
Rancho-Lee <hairdrap7@gmail.com>
2022-12-09 14:36:08 -0800

    s

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+for num in range(100):
+    if(num % 3 == 0 and num % 5 == 0):
+        print("Fizz Buzz")
+    elif(num % 3 == 0):
+        print("Fizz")
+    elif(num % 5 == 0):
+        print("Buzz")
+    else:
+        print(num)
