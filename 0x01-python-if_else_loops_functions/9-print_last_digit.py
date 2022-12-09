d38a46d317d732b98a225dc3a2d2659b1f8b11e0
Rancho-Lee <hairdrap7@gmail.com>
2022-12-09 14:14:10 -0800

    j

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+    if(number < 0):
+        Last_digit_number = (number % -10) * -1
+    else:
+        Last_digit_number = number % 10
+    print(Last_digit_number)
+    return Last_digit_number
